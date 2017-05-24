7503d5f8d19ebc2383da077de788779803529870
MattdHall <matthew.hall@monash.edu>
2017-05-24 17:55:57 +1000

    Add dot size option

--- R/plot_sdr_blank.R
+++ R/plot_sdr_blank.R
@@ -14,11 +14,11 @@
 #' @examples
 #' plot_metab_blanks(test_data_merged)
 
-plot_sdr_blanks <- function(metab_df){
+plot_sdr_blanks <- function(metab_df, dotcex = 0.6){
 
   p <- metab_df %>% mutate(Blank = ifelse(Indiv_ID == "BLANK", "Y", "N")) %>%
     ggplot(aes(x = 1, y = slope, fill = Blank)) + facet_grid(. ~ SDR) +
-    geom_dotplot(binaxis = "y", stackdir = "center", dotsize = 0.8, binwidth = 0.01) +
+    geom_dotplot(binaxis = "y", stackdir = "center", dotsize = dotcex, binwidth = 0.01) +
     theme_bw() + ggtitle("Variation in slopes for BLANK cells for each SDR") +
     theme(plot.title = element_text(hjust = 0.5)) +
     theme(axis.ticks = element_blank(), axis.text.x = element_blank(), axis.title = element_blank())
