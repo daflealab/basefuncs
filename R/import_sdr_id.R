cf816a633d1604f8837f8f2387a2962b35f4695d
MattdHall <matthew.hall@monash.edu>
2017-05-24 15:58:28 +1000

    Fix import_sdr_id function
    Added error checking for missing data or duplciate SDRs. Now requires users to remove blank SDR entries (A1 to D6) before import.

--- R/import_sdr_id.R
+++ R/import_sdr_id.R
@@ -1,16 +1,19 @@
 #' Imports IDs associated with a SDR run
 #'
 #' \code{import_sdr_id} imports an excel file linking the
-#' location on an SDR to a ID:
-#' The last letter of the Run_ID must be a letter
-#' The last three digits of the SDR_ID must be the SDR number
+#' location on an SDR to a ID. SDR ID must be a number.
+#' Unused SDRs must be removed from the excel file (No
+#' blank SDRs). Blank or Empty cells must be named Explicitly.
 #'
-#' @param file_name A string. The excel file to import.
+#' @param file_name A string. The excel file to import. Must contain the
+#' DATE in cell B2, BLOCK in B3 and RUN in B4. First 5 rows will be
+#'  removed after import.
 #' @param sheet_number An integer. The sheet to import from
 #' the excel workbook. Indexed by number. Default is the first sheet.
 #'
 #' @return A tibble with Date, Run, SDR, Cell Details, and Animal IDs.
-#' Will convert b or B or Blank or blank to BLANK
+#' Will convert b or B or Blank or blank to BLANK. Returns an error
+#' if duplicate SDR IDs are detected or unnames cells incuded.
 #' @export
 #'
 #' @examples
@@ -18,32 +21,55 @@
 
 import_sdr_id <- function(file_name, sheet_number = 1){
 
-  test_L <- readxl::read_excel(file_name, sheet = sheet_number, range = cell_cols("A:G"),
-                       col_names = c("Row_ID", "R1", "R2", "R3", "R4", "R5", "R6")) %>%
-    mutate(Date = first(Row_ID)) %>% mutate(Run = first(R1)) %>%
-    slice(-1)
+  test_L <- readxl::read_excel(path = file_name, sheet = sheet_number, range = cell_cols("A:G"),
+                               col_names = c("Row_ID", "R1", "R2", "R3", "R4", "R5", "R6"),
+                               col_types = "text") %>% slice(-(1:5))
 
-  test_R <- readxl::read_excel(file_name, sheet = sheet_number, range = cell_cols("I:O"),
-                       col_names = c("Row_ID", "R1", "R2", "R3", "R4", "R5", "R6")) %>%
-    mutate(Date = first(Row_ID)) %>% mutate(Run = first(R1)) %>%
-    slice(-1)
+  test_R <- readxl::read_excel(path = file_name, sheet = sheet_number, range = cell_cols("I:O"),
+                               col_names = c("Row_ID", "R1", "R2", "R3", "R4", "R5", "R6"),
+                               col_types = "text")
 
-  test_LR <- test_L %>% bind_rows(test_R)
+  date <- readxl::read_excel(path = file_name, range = "B2",
+                             col_names = FALSE, sheet = sheet_number)[[1]]
+  block <- readxl::read_excel(path = file_name, range = "B3",
+                              col_names = FALSE, sheet = sheet_number)[[1]]
+  run <- readxl::read_excel(path = file_name, range = "B4",
+                            col_names = FALSE, sheet = sheet_number)[[1]]
+
+  test_LR <- test_L %>% bind_rows(test_R) %>%
+    mutate(Date = date, Block = block, Run = run) %>%
+    na.omit()
 
   #Requires that SDR cells contain the text SDR followed by a space or underscore
-  SDR_id <- test_LR %>% select(R1) %>% filter(stringr::str_detect(R1, "SDR")) %>% unlist(use.names = FALSE)
+  SDR_id <- test_LR %>% select(Row_ID) %>%
+    filter(!(Row_ID %in% c("A", "B", "C", "D"))) %>%
+    unlist(use.names = FALSE)
 
-  test_LR_trim <- test_LR %>% filter(!is.na(Row_ID)) %>% filter(!is.na(R1)) %>%
-    mutate(SDR = rep(SDR_id, each = 4)) %>%
-    select(Date, Run, SDR, Row_ID, everything()) %>%
-    gather(Column_ID, Indiv_ID, R1:R6) %>%
-    mutate(Column_ID = stringr::str_sub(Column_ID, -1)) %>%
-    mutate(SDR = stringr::str_sub(SDR, -3)) %>%
-    mutate(Run = stringr::str_sub(Run, -1)) %>%
-    unite(col = Cell, Row_ID, Column_ID, remove = FALSE, sep = "") %>%
-    unite(location_ID, SDR, Cell, sep = "_", remove = FALSE) %>%
-    mutate(Indiv_ID = ifelse(Indiv_ID %in% c("B", "b", "Blank", "blank"), "BLANK", Indiv_ID)) %>%
-    select(location_ID, SDR, Run, Cell, everything())
+  if (length(unique(SDR_id)) != length(SDR_id))
+  {
 
-  return(test_LR_trim)
+    stop("Duplicate SDR IDs in file")
+
+  } else if (nrow(test_LR)/length(SDR_id) != 5)
+  {
+
+    stop("Missing values in SDR cells")
+
+  } else
+  {
+
+    test_LR_trim <- test_LR %>% filter(Row_ID %in% c("A", "B", "C", "D")) %>%
+      mutate(SDR = rep(SDR_id, each = 4)) %>%
+      select(Date, Run, SDR, Row_ID, everything()) %>%
+      gather(Column_ID, Indiv_ID, R1:R6) %>%
+      mutate(Column_ID = str_sub(Column_ID, -1)) %>%
+      unite(col = Cell, Row_ID, Column_ID, remove = FALSE, sep = "") %>%
+      unite(location_ID, SDR, Cell, sep = "_", remove = FALSE) %>%
+      mutate(Indiv_ID = ifelse(Indiv_ID %in% c("B", "b", "Blank", "blank"), "BLANK", Indiv_ID)) %>%
+      mutate(Date = lubridate::as_date(Date)) %>%
+      select(Date, Block, Run, location_ID, SDR, Run, Cell, everything())
+
+    return(test_LR_trim)
+
+  }
 }
