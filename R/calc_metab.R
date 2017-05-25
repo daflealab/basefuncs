a52fd7ee93251d598957860355c76b16a7794f71
mattdhall <matthew.hall@monash.edu>
2017-05-25 11:26:02 +1000

    Convert slope from time_min to time_hours

--- R/calc_metab.R
+++ R/calc_metab.R
@@ -3,14 +3,16 @@
 #' \code{calc_metab} takes the LoLinR method of
 #' estimating the best fitting change in oxygen consumption
 #' but applies this to a nested dataframe of many SDR plate runs.
+#' Time is converted from min to hours automatically.
 #'
 #' @param nested_df The nested df imported using import_metab().
+#' Time must still be in minutes (e.g. Time_min)
 #' @param metab_method A string. Either "pc" or "eq".
 #' "pc"" is the suggested method by Colin for oxygen consumption data.
 #' It is the default.
 #' @param alpha_value Default at 0.2.
 #'
-#' @return A nested tibble with the model, summary and slopes as list columns
+#' @return A nested tibble with the model, summary and slopes (per hour change) as list columns.
 #' @export
 #'
 #' @examples
@@ -20,7 +22,7 @@ calc_metab <- function(nested_df, metab_method = "pc", alpha_value = 0.2) {
 
   metab_model <- function(df) {
     #Function to run the oxygen comsumption models
-    LoLinR::rankLocReg(xall = df$Time_min, yall = df$V02, alpha = alpha_value, method = metab_method, verbose = FALSE)
+    LoLinR::rankLocReg(xall = (df$Time_min/60), yall = df$V02, alpha = alpha_value, method = metab_method, verbose = FALSE)
   }
 
   get_beta <- function(summary_table) {
